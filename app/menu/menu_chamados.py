4b010b9fa43ee2efe4749af7896014f683bddb5e
Daniel Bonin <danielbonin0011@gmail.com>
2026-09-01 11:16:31 -0300

    Definição dos métodos das tabelas do microsserviço, também houve modificações no session devido o crescimento da versão 1.0

---
+++ app/menu/menu_chamados.py
@@ -0,0 +1,8 @@
+def menu_principal():
+    print('SISTEMA DE ATENDIMENTO')
+    print('1 - Usuários')
+    print('2 - Técnicos')
+    print('3 - Grupos Técnicos')
+    print('4 - Chamados')
+    print('5 - Relatórios')
+    print('0 - Sair')
