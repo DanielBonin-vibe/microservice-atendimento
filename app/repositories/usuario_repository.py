4dc39e652a9cc98fba81598c3f6ea056d3303232
Daniel Bonin <danielbonin0011@gmail.com>
2026-09-09 11:15:09 -0300

    Refatoração de usuario_repository para o recebiemnto da classe UsuarioRepository e seguir os padrões de POO.

--- app/repositories/usuario_repository.py
+++ app/repositories/usuario_repository.py
@@ -1,225 +1,233 @@
 from app.database.session import conectar
 
-def cadastro_usuario(nome, cpf_usuario, email, telefone, senha_hash):
-    conexao = conectar()
+class UsuarioRepository:
 
-    try:
-        cursor = conexao.cursor()
+    def __init__(self, conectar):
+        self.conectar_banco = conectar
 
-        cursor.execute("""
-        INSERT INTO usuarios (nome, cpf_usuario, email, telefone, senha_hash)
-        VALUES (%s, %s, %s, %s, %s)
-        """, (nome, cpf_usuario, email, telefone, senha_hash))
+    def cadastro_usuario(self, nome, cpf_usuario, email, telefone, senha_hash):
+        conexao = self.conectar_banco()
 
-        resultado = cursor.rowcount
+        try:
+            cursor = conexao.cursor()
 
-        if resultado > 0:
-            conexao.commit()
+            cursor.execute("""
+            INSERT INTO usuarios (nome, cpf_usuario, email, telefone, senha_hash)
+            VALUES (%s, %s, %s, %s, %s)
+            """, (nome, cpf_usuario, email, telefone, senha_hash))
+
+            resultado = cursor.rowcount
+
+            if resultado > 0:
+                conexao.commit()
+                return resultado
+
+            conexao.rollback()
+            return 0
+
+        except Exception as erro:
+            conexao.rollback()
+            print(f'Erro ao cadastrar usuário: {erro}')
+            return 0
+        finally:
+            cursor.close()
+            conexao.close()
+
+    def listar_usuarios(self):
+        conexao = self.conectar_banco()
+
+        try:
+            cursor = conexao.cursor()
+
+            cursor.execute("""
+            SELECT * FROM usuarios
+            """)
+
+            resultado = cursor.fetchall()
+
             return resultado
 
-        conexao.rollback()
-        return 0
+        except Exception as erro:
+            print(f'Erro ao listar usuários: {erro}')
+            return None
+        
+        finally:
+            cursor.close()
+            conexao.close()
 
-    except Exception as erro:
-        conexao.rollback()
-        print(f'Erro ao cadastrar usuário: {erro}')
-        return 0
-    finally:
-        cursor.close()
-        conexao.close()
+    def buscar_usuario(self, busca):
+        conexao = self.conectar_banco()
 
-def listar_usuarios():
-    conexao = conectar()
+        try:
+            cursor = conexao.cursor()
 
-    try:
-        cursor = conexao.cursor()
+            cursor.execute("""
+            SELECT * FROM usuarios
+            WHERE nome ILIKE %s
+                OR cpf_usuario ILIKE %s
+                OR email ILIKE %s
+                OR telefone ILIKE %s
+            """, (f'%{busca}%', f'%{busca}%', f'%{busca}%', f'%{busca}%'))
 
-        cursor.execute("""
-        SELECT * FROM usuarios
-        """)
+            resultado = cursor.fetchall()
 
-        resultado = cursor.fetchall()
-
-        return resultado
-
-    except Exception as erro:
-        print(f'Erro ao cadastrar usuário: {erro}')
-        return None
-    
-    finally:
-        cursor.close()
-        conexao.close()
-
-def buscar_usuario(busca):
-    conexao = conectar()
-
-    try:
-        cursor = conexao.cursor()
-
-        cursor.execute("""
-        SELECT * FROM usuarios
-        WHERE nome ILIKE %s
-            OR cpf_usuario ILIKE %s
-            OR email ILIKE %s
-            OR telefone ILIKE %s
-        """, (f'%{busca}%', f'%{busca}%', f'%{busca}%', f'%{busca}%'))
-
-        resultado = cursor.fetchall()
-
-        return resultado
-
-    except Exception as erro:
-        print(f'Erro ao buscar usuário: {erro}')
-    finally:
-        cursor.close()
-        conexao.close()
-
-def buscar_usuario_cpf(cpf_usuario):
-    conexao = conectar()
-
-    try:
-        cursor = conexao.cursor()
-
-        cursor.execute("""
-        SELECT * FROM usuarios
-        WHERE cpf_usuario = %s
-        """, (cpf_usuario))
-
-        resultado = cursor.fetchone()
-
-        if resultado > 0:
             return resultado
 
-        return 0
+        except Exception as erro:
+            print(f'Erro ao buscar usuário: {erro}')
+        finally:
+            cursor.close()
+            conexao.close()
 
-    except Exception as erro:
-        print(f'Erro ao buscar usuário pelo CPF: {erro}')
-        return 0
-    
-    finally:
-        cursor.close()
-        conexao.close()    
+    def buscar_usuario_cpf(self, cpf_usuario):
+        conexao = self.conectar_banco()
 
-def pesquisar_usuarios(nome=None, cpf_usuario=None, email=None, telefone=None):
-    conexao = conectar()
+        try:
+            cursor = conexao.cursor()
 
-    try:
-        cursor = conexao.cursor()
+            cursor.execute("""
+            SELECT * FROM usuarios
+            WHERE cpf_usuario = %s
+            """, (cpf_usuario,))
 
-        parametros = (
-        nome, f'%{nome}%' if nome else None,
-        cpf_usuario, f'%{cpf_usuario}%' if cpf_usuario else None,
-        email, f'%{email}%' if email else None,
-        telefone, f'%{telefone}%' if telefone else None)   
+            resultado = cursor.fetchone()
 
-        cursor.execute("""
-        SELECT * FROM usuarios
-        WHERE (%s IS NULL OR nome ILIKE %s)
-            AND (%s IS NULL OR cpf_usuario ILIKE %s)
-            AND (%s IS NULL OR email ILIKE %s)
-            AND (%s IS NULL OR telefone ILIKE %s)
-            """, (parametros))
+            if resultado is not None:
+                return resultado
 
-        resultado = cursor.fetchall()
+            return None
 
-        return resultado
+        except Exception as erro:
+            print(f'Erro ao buscar usuário pelo CPF: {erro}')
+            return None
+        
+        finally:
+            cursor.close()
+            conexao.close()    
 
-    except Exception as erro:
-        print(f'Erro ao pesquisar usuário: {erro}')
-        return None
-    
-    finally:
-        cursor.close()
-        conexao.close()
+    def pesquisar_usuarios(self, nome=None, cpf_usuario=None, email=None, telefone=None):
+        conexao = self.conectar_banco()
 
+        try:
+            cursor = conexao.cursor()
 
-def atualizar_usuario(cpf_usuario_inicial, nome=None, cpf_usuario_novo=None, email=None, telefone=None):
-    conexao = conectar()
+            parametros = (
+            nome, f'%{nome}%' if nome else None,
+            cpf_usuario, f'%{cpf_usuario}%' if cpf_usuario else None,
+            email, f'%{email}%' if email else None,
+            telefone, f'%{telefone}%' if telefone else None)   
 
-    try:
-        cursor = conexao.cursor()
+            cursor.execute("""
+            SELECT * FROM usuarios
+            WHERE (%s IS NULL OR nome ILIKE %s)
+                AND (%s IS NULL OR cpf_usuario ILIKE %s)
+                AND (%s IS NULL OR email ILIKE %s)
+                AND (%s IS NULL OR telefone ILIKE %s)
+                """, parametros)
 
-        cursor.execute("""
-        UPDATE usuarios
-        SET nome = COALESCE(%s, nome),
-            cpf_usuario = COALESCE(%s, cpf_usuario),
-            email = COALESCE(%s, email),
-            telefone = COALESCE(%s, telefone)
-        WHERE cpf_usuario = %s
-        """, (nome, cpf_usuario_novo, email, telefone, cpf_usuario_inicial))
+            resultado = cursor.fetchall()
 
-        resultado = cursor.rowcount
-
-        if resultado > 0:
-            conexao.commit()
             return resultado
 
-        conexao.rollback()
-        return 0
+        except Exception as erro:
+            print(f'Erro ao pesquisar usuário: {erro}')
+            return None
+        
+        finally:
+            cursor.close()
+            conexao.close()
 
-    except Exception as erro:
-        conexao.rollback()
-        print(f'Erro ao atualizar usuário: {erro}')
-        return 0
-    
-    finally:
-        cursor.close()
-        conexao.close()
 
-def alterar_senha_usuario(senha_hash_nova, cpf_usuario):
-    conexao = conectar()
+    def atualizar_usuario(self, cpf_usuario_inicial, nome=None, cpf_usuario_novo=None, email=None, telefone=None):
+        conexao = self.conectar_banco()
 
-    try:
-        cursor = conexao.cursor()
+        try:
+            cursor = conexao.cursor()
 
-        cursor.execute("""
-        UPDATE usuarios
-        SET senha_hash = %s
-        WHERE cpf_usuario = %s
-        """, (senha_hash_nova, cpf_usuario))
+            cursor.execute("""
+            UPDATE usuarios
+            SET nome = COALESCE(%s, nome),
+                cpf_usuario = COALESCE(%s, cpf_usuario),
+                email = COALESCE(%s, email),
+                telefone = COALESCE(%s, telefone)
+            WHERE cpf_usuario = %s
+            """, (nome, cpf_usuario_novo, email, telefone, cpf_usuario_inicial))
 
-        resultado = cursor.rowcount
+            resultado = cursor.rowcount
 
-        if resultado > 0:
-            return resultado
+            if resultado > 0:
+                conexao.commit()
+                return resultado
 
-        conexao.rollback()
-        return 0
+            conexao.rollback()
+            return 0
 
-    except Exception as erro:
-        conexao.rollback()
-        print(f'Erro ao aletarr senha: {erro}')
-        return 0
-    
-    finally:
-        cursor.close()
-        conexao.close()
+        except Exception as erro:
+            conexao.rollback()
+            print(f'Erro ao atualizar usuário: {erro}')
+            return 0
+        
+        finally:
+            cursor.close()
+            conexao.close()
 
-def excluir_usuario(cpf_usuario):
-    conexao = conectar()
+    def alterar_senha_usuario(self, senha_hash_nova, cpf_usuario):
+        conexao = self.conectar_banco()
 
-    try:
-        cursor = conexao.cursor()
+        try:
+            cursor = conexao.cursor()
 
-        cursor.execute("""
-        DELETE FROM usuarios
-        WHERE cpf_usuario = %s
-        """, (cpf_usuario,))
+            cursor.execute("""
+            UPDATE usuarios
+            SET senha_hash = %s
+            WHERE cpf_usuario = %s
+            """, (senha_hash_nova, cpf_usuario))
 
-        resultado = cursor.rowcount
+            resultado = cursor.rowcount
 
-        if resultado > 0:
-            conexao.commit()
-            return resultado
+            if resultado > 0:
+                conexao.commit()
+                return resultado
 
-        conexao.rollback()
-        return 0
+            conexao.rollback()
+            return 0
 
-    except Exception as erro:
-        conexao.rollback()
-        print(f'Erro ao excluir usuário: {erro}')
-        return 0
-    
-    finally:
-        cursor.close()
-        conexao.close()
+        except Exception as erro:
+            conexao.rollback()
+            print(f'Erro ao aletarr senha: {erro}')
+            return 0
+        
+        finally:
+            cursor.close()
+            conexao.close()
+
+    def excluir_usuario(self, cpf_usuario):
+        conexao = self.conectar_banco()
+
+        try:
+            cursor = conexao.cursor()
+
+            cursor.execute("""
+            DELETE FROM usuarios
+            WHERE cpf_usuario = %s
+            """, (cpf_usuario,))
+
+            resultado = cursor.rowcount
+
+            if resultado > 0:
+                conexao.commit()
+                return resultado
+
+            conexao.rollback()
+            return 0
+
+        except Exception as erro:
+            conexao.rollback()
+            print(f'Erro ao excluir usuário: {erro}')
+            return 0
+        
+        finally:
+            cursor.close()
+            conexao.close()
+
+usuario_repository = UsuarioRepository(conectar)
